3fe7725b05eb4e84bbbf6863918594add48a86a0
giovannijunseokim <kingjunseo@gmail.com>
2024-03-12 15:51:15 +0900

    feat: finish after navigation

--- app/src/main/java/univ/earthbreaker/namu/feature/login/LoginActivity.kt
+++ app/src/main/java/univ/earthbreaker/namu/feature/login/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import univ.earthbreaker.namu.MainActivity
+import univ.earthbreaker.namu.util.finishSafely
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +17,6 @@ class LoginActivity : ComponentActivity() {
 
     private val navigateToMain: () -> Unit = {
         startActivity(Intent(this, MainActivity::class.java))
+        finishSafely()
     }
 }
