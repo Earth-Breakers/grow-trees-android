015f2b5e5c4a854419407e14d50803ee5e75b15b
giovannijunseokim <kingjunseo@gmail.com>
2024-03-12 18:53:42 +0900

    feat: init HomeActivity

---
+++ app/src/main/java/univ/earthbreaker/namu/feature/home/HomeActivity.kt
@@ -0,0 +1,14 @@
+package univ.earthbreaker.namu.feature.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HomeScreen()
+        }
+    }
+}
