e8fe5f1d67a24a8760d3dace6d8c0449c20d1d50
giovannijunseokim <kingjunseo@gmail.com>
2024-03-12 18:55:41 +0900

    chore: modify navigateToMain to navigateToHome

--- app/src/main/java/univ/earthbreaker/namu/feature/login/LoginActivity.kt
+++ app/src/main/java/univ/earthbreaker/namu/feature/login/LoginActivity.kt
@@ -4,19 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import univ.earthbreaker.namu.MainActivity
+import univ.earthbreaker.namu.feature.home.HomeActivity
 import univ.earthbreaker.namu.util.finishSafely
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LoginScreen(onClickLoginButton = navigateToMain)
+            LoginScreen(onClickLoginButton = navigateToHome)
         }
     }
 
-    private val navigateToMain: () -> Unit = {
-        startActivity(Intent(this, MainActivity::class.java))
+    private val navigateToHome: () -> Unit = {
+        startActivity(Intent(this, HomeActivity::class.java))
         finishSafely()
     }
 }
