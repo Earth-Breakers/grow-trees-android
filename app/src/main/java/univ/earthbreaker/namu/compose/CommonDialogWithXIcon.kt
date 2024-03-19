7ecef88e457c5d6ab13fd334b278cb54f13bac93
giovannijunseokim <kingjunseo@gmail.com>
2024-03-19 14:45:48 +0900

    chore: remove ripple with box

--- app/src/main/java/univ/earthbreaker/namu/compose/CommonDialogWithXIcon.kt
+++ app/src/main/java/univ/earthbreaker/namu/compose/CommonDialogWithXIcon.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -48,12 +47,13 @@ fun CommonDialogWithXIcon(
             }
 
             Spacer(Modifier.height(23.dp))
-            IconButton(
+            Box(
                 modifier = Modifier
                     .size(size = 64.dp)
                     .clip(shape = RoundedCornerShape(100.dp))
-                    .background(GTTheme.colors.white),
-                onClick = dismiss,
+                    .background(GTTheme.colors.white)
+                    .noRippleClickable(onClick = dismiss),
+                contentAlignment = Alignment.Center,
             ) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_x),
